5e3687722f7b47297ce85ee40fea7a4b954ad0bb
Ali Clark <ali@clark.gb.net>
2016-08-21 11:53:46 +0100

    fix, add docker debian jessie repo to sources.list

--- shadow-tor-docker/install.1.sh
+++ shadow-tor-docker/install.1.sh
@@ -3,9 +3,10 @@
 
 apt-get install -y apt-transport-https
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo debian-jessie main' >>/etc/apt/sources.list
 apt-get update
 
 apt-get install -y docker-engine
 
-docker build -t shadow .
-docker run shadow
+docker build -t shadow-quux .
+docker run shadow-quux
