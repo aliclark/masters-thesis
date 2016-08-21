9b1bb647e32f9dad4c410472e5b16b2e660bd872
Ali Clark <ali@clark.gb.net>
2016-08-22 00:08:02 +0100

    install basic-min for up to 8 clients

--- chutney-atlantic/install.sh
+++ chutney-atlantic/install.sh
@@ -6,8 +6,14 @@ set -e
 NEWARK=45.79.174.25
 LONDON=139.162.224.204
 
+
+# TODO: all the stuff that comes before
+
+
 scp configure-chutney-newark.sh root@$NEWARK:
 scp configure-chutney-london.sh root@$LONDON:
 
-ssh root@$NEWARK "sed 's/Client.getN(1)/Client.getN(2)/' chutney/networks/basic-min >chutney/networks/basic-min2; sed 's/Client.getN(1)/Client.getN(3)/' chutney/networks/basic-min >chutney/networks/basic-min3"
-ssh root@$LONDON "sed 's/Client.getN(1)/Client.getN(2)/' chutney/networks/basic-min >chutney/networks/basic-min2; sed 's/Client.getN(1)/Client.getN(3)/' chutney/networks/basic-min >chutney/networks/basic-min3"
+for i in $(seq 1 8); do
+    ssh root@$NEWARK "sed 's/Client.getN(1)/Client.getN($i)/' chutney/networks/basic-min >chutney/networks/basic-min-$i"
+    ssh root@$LONDON "sed 's/Client.getN(1)/Client.getN($i)/' chutney/networks/basic-min >chutney/networks/basic-min-$i"
+done
