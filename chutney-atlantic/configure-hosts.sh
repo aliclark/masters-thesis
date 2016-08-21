990dae708e7aa257dbe10160f59eed32f0d94138
Ali Clark <ali@clark.gb.net>
2016-08-22 00:09:35 +0100

    allow experiment to be configured, run it from london node

--- chutney-atlantic/configure-hosts.sh
+++ chutney-atlantic/configure-hosts.sh
@@ -3,10 +3,10 @@
 set -x
 set -e
 
-network=basic-min2
+network=$1
 
 NEWARK=45.79.174.25
 LONDON=139.162.224.204
 
 ssh root@$NEWARK "./configure-chutney-newark.sh $network 1>&2 && grep -E '^DirAuthority ' chutney/net/nodes/000*/torrc" \
-    | ssh root@$LONDON "./configure-chutney-london.sh $network"
+    | ./configure-chutney-london.sh $network
--- chutney-atlantic/experiment.sh
+++ chutney-atlantic/experiment.sh
@@ -3,28 +3,40 @@
 set -x
 set -e
 
-network=basic-min2
+branch=$1
+network=$2
+loss=$3
+extra=$4
 
 NEWARK=45.79.174.25
-LONDON=139.162.224.204
+
+echo experiment version: 1 branch: $branch network: $network loss: $loss extra: $extra
 
 # clear out any cached state from previous runs
-./configure-hosts.sh
+./configure-hosts.sh $network
 
-ssh root@$NEWARK "cd chutney; CHUTNEY_TOR=/root/tor/src/or/tor ./chutney start networks/$network"
-ssh root@$LONDON "tc qdisc del dev eth0 root netem || true; cd chutney; CHUTNEY_TOR=/root/tor/src/or/tor ./chutney start networks/$network"
+cd $HOME/chutney
+
+ssh root@$NEWARK "cd chutney; CHUTNEY_TOR=/root/$branch/src/or/tor ./chutney start networks/$network"
+
+tc qdisc del dev eth0 root netem || true
+CHUTNEY_TOR=/root/$branch/src/or/tor ./chutney start networks/$network
 
 # minimum sleep before anything useful could happen
 sleep 15
 
 # just one run to set up a circuit
-ssh root@$LONDON "cd chutney; ./chutney verify networks/$network"
+./chutney verify networks/$network
 
 # packet loss
-#ssh root@$LONDON "tc qdisc add dev eth0 root netem loss 12.5%"
+if [ "$loss" ]; then
+    tc qdisc add dev eth0 root netem loss $loss
+fi
 
-# 128 MiB
-ssh root@$LONDON "cd chutney; CHUTNEY_DATA_BYTES=134217728 ./chutney verify networks/$network"
+# 64 MiB
+CHUTNEY_DATA_BYTES=67108864 ./chutney verify networks/$network
 
+# TODO: put these in a trap exit
 ssh root@$NEWARK "cd chutney; ./chutney stop networks/$network; killall -9 tor || true"
-ssh root@$LONDON "cd chutney; ./chutney stop networks/$network; killall -9 tor || true"
+./chutney stop networks/$network
+killall -9 tor || true
