e50eace0a9178ec43fac18eeca8b3e2e18289daa
Ali Clark <ali@clark.gb.net>
2016-08-23 17:50:16 +0100

    ensure we don't segment outbound packets so dropping them isn't unusually bad

--- chutney-atlantic/configure-chutney-newark.sh
+++ chutney-atlantic/configure-chutney-newark.sh
@@ -7,6 +7,12 @@ network=$1
 
 killall -9 tor || true
 
+# Don't give outbound packets any special segmentation treatment
+ethtool -K eth0 tso off
+ethtool -K eth0 gso off
+ethtool -K eth0 gro off
+ethtool -K eth0 ufo off
+
 cd $HOME/chutney
 
 CHUTNEY_LISTEN_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com) CHUTNEY_TOR_GENCERT=/root/tor-0.2.7.6/src/tools/tor-gencert CHUTNEY_TOR=/root/tor-0.2.7.6/src/or/tor ./chutney configure networks/$network
