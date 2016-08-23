9851c68451e0d4c4ae03918f17361d31df08ed8a
Ali Clark <ali@clark.gb.net>
2016-08-23 17:51:05 +0100

    fix the install script a bit

--- chutney-atlantic/install.sh
+++ chutney-atlantic/install.sh
@@ -6,14 +6,8 @@ set -e
 NEWARK=45.79.174.25
 LONDON=139.162.224.204
 
+scp install-newark.sh configure-chutney-newark.sh root@$NEWARK:
+scp install-london.sh configure-chutney-london.sh root@$LONDON:
 
-# TODO: all the stuff that comes before
-
-
-scp configure-chutney-newark.sh root@$NEWARK:
-scp configure-chutney-london.sh root@$LONDON:
-
-for i in $(seq 1 8); do
-    ssh root@$NEWARK "sed 's/Client.getN(1)/Client.getN($i)/' chutney/networks/basic-min >chutney/networks/basic-min-$i"
-    ssh root@$LONDON "sed 's/Client.getN(1)/Client.getN($i)/' chutney/networks/basic-min >chutney/networks/basic-min-$i"
-done
+ssh root@$NEWARK ./install-newark.sh
+ssh root@$LONDON ./install-london.sh
