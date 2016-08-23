b79fc9b662ce431d08c581a7bb6bce3b6cbdc48a
Ali Clark <ali@clark.gb.net>
2016-08-23 18:02:31 +0100

    add the host install scripts

---
+++ chutney-atlantic/install-newark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+NEWARK=45.79.174.25
+LONDON=139.162.224.204
+
+apt-get update -y
+#apt-get upgrade -y
+
+apt-get install -y ethtool
+
+ip6tables -F INPUT
+ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -P INPUT DROP
+
+iptables -F INPUT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 5125 -j ACCEPT
+iptables -A INPUT -s $LONDON -j ACCEPT
+iptables -P INPUT DROP
+
+for i in $(seq 1 8); do
+    sed "s/Client.getN(1)/Client.getN($i)/" chutney/networks/basic-min >chutney/networks/basic-min-$i
+done
