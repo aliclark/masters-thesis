ab8710243bffd43e9b5a454c17ca8b943d1782d8
Ali Clark <ali@clark.gb.net>
2016-08-25 20:40:16 +0100

    bump the sleep time to see if it helps

--- chutney-atlantic/experiment.sh
+++ chutney-atlantic/experiment.sh
@@ -23,7 +23,7 @@ tc qdisc del dev eth0 root netem || true
 CHUTNEY_TOR=/root/$branch/src/or/tor ./chutney start networks/$network
 
 # minimum sleep before anything useful could happen
-sleep 15
+sleep 35
 
 # just one run to set up a circuit
 ./chutney verify networks/$network
