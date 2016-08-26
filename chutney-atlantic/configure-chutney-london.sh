e0d6e88fcdc9faf259575174ab39c469a5ce3308
Ali Clark <ali@clark.gb.net>
2016-08-26 23:42:38 +0100

    ensure only one node is used as middle

--- chutney-atlantic/configure-chutney-london.sh
+++ chutney-atlantic/configure-chutney-london.sh
@@ -16,9 +16,17 @@ CHUTNEY_LISTEN_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com) CHU
 sed -i 's/^Log/#Log/' net/nodes/*/torrc
 
 # Point all nodes to the authorities provided, and not the ones generated above
+guardip=''
 while read line; do
+  if [ ! "$guardip" ]; then
+    guardip=$(grep -E '^DirAuthority ' net/nodes/000*/torrc | awk '{print $7}' | awk -F: '{print $1}')
+  fi
   sed -i "s/^$(echo $line | awk '{print $1" "$2}') .*/$line/" net/nodes/*/torrc
 done
+
+# Make sure only one of the two auths is used as guard.
+# The other is therefore used as middle, so there is only one middle->exit connection
+echo "ReachableORAddresses $guardip:5000" | tee -a net/nodes/*c/torrc
 
 # No authorities this side - ensures we use them as Guard,Middle
 # across the pond
